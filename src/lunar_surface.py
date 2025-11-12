5baa528bef0e43370fe0933a75dbf04c267d203f
dio067 <yousefnas06@gmail.com>
2025-11-12 14:51:59 +0400

    Initial commit

---
+++ src/lunar_surface.py
@@ -0,0 +1,39 @@
+import pandas as pd;
+import numpy as np;
+import networkx as nx;
+import matplotlib.pyplot as plt;
+
+
+# Load lunar surface site data and satellite data
+sites = pd.read_csv('../data/surface_sites.csv');
+sats = pd.read_csv('../data/satellite_data.csv');
+
+
+
+# Create a graph
+G = nx.Graph();
+
+
+# Add Surface Sites Nodes
+for _, row in sites.iterrows():
+   G.add_node(row['name'], type=row['type'], lat=row['lat'], lon=row['lon'])
+
+
+# Add Satellites Nodes
+for _, row in sats.iterrows():
+    G.add_node(row['name'], pos=(row['lon'], row['lat']), type='satellite');
+
+# Add edges Surface 
+  for i, site1 in sites.iterrows():
+    for j, site2 in sites.iterrows():
+        dist = np.sqrt((site1.lat - site2.lat)**2 + (site1.lon - site2.lon)**2)
+          if dist < 80:
+            G.add_edge(site1.name, site2.name, weight=dist)
+
+
+# Add edges Satellite 
+for _, sat1 in sats.iterrows():
+    for _, site in sites.iterrows():
+        dist = np.sqrt((sat.altitude_km)**2 + (site.lat - site.lat)**2 + (site.lon - site.lon)**2)
+        if dist <= sat.coverage_radius_km:
+            G.add_edge(sat.name, site.name, weight=dist)
